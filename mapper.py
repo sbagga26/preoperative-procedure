d18f9ebbf75adfb431d1b24bfa739817543b04de
sbagga26 <49098552+sbagga26@users.noreply.github.com>
2019-03-30 21:51:40 +0530

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,14 +1,17 @@
+#this program is basically for finding out the pre-operative procedure that needs to be performed according
+#to the data provided by the user and using our database to find out 
+
 #!/usr/bin/python36
 
 import xlrd
 import pandas as pd
 from variables import *
-df=pd.read_excel('/root/Desktop/Project/Metadata.xlsx',sheet_name='Surgical Grade {}'.format(sg))
+df=pd.read_excel('/root/Desktop/Project/Metadata.xlsx',sheet_name='Surgical Grade {}'.format(sg)) #edit the location of your metafile downloaded
 col=df[age]=='Y'
 print (df[col]['Test'])
 
 if diabetes=='ON' or hypertension=='ON' or asthma=='ON' or thyroid=='ON' or copd=='ON' or meds=='ON':
-	df=pd.read_excel('/root/Desktop/Project/Metadata.xlsx',sheet_name='Chronic Illness')
+	df=pd.read_excel('/root/Desktop/Project/Metadata.xlsx',sheet_name='Chronic Illness') #edit the location of your metafile downloaded
 
 	if diabetes=='ON':
 		col=df['Diabetes']=='Y'
@@ -30,7 +33,7 @@ if diabetes=='ON' or hypertension=='ON' or asthma=='ON' or thyroid=='ON' or copd
 		print(df[col]['Test'])
 
 elif asa==2:
-	df=pd.read_excel('/root/Desktop/Project/Metadata.xlsx',sheet_name='ASA 2')
+	df=pd.read_excel('/root/Desktop/Project/Metadata.xlsx',sheet_name='ASA 2') #edit the location of your metafile downloaded
 	if reg=='cvs':
 		col=df['CVS']=='Y'
 		print(df[col]['Test'])
@@ -42,7 +45,7 @@ elif asa==2:
 		print(df[col]['Test'])
 
 elif asa==3:
-	df=pd.read_excel('/root/Desktop/Project/Metadata.xlsx',sheet_name='ASA 3')
+	df=pd.read_excel('/root/Desktop/Project/Metadata.xlsx',sheet_name='ASA 3') #edit the location of your metafile downloaded
 	if reg=='cvs':
 		col=df['CVS']=='Y'
 		print(df[col]['Test'])
